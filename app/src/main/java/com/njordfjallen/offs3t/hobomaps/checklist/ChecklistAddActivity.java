efb27f363b51a1c562ee2fbc1649d70b6ace9c8a
offs3t <quavoyrn2016@gmail.com>
2016-06-08 15:57:24 -0700

    Write header comment and add constant values to ChecklistAddActivity

--- app/src/main/java/com/njordfjallen/offs3t/hobomaps/checklist/ChecklistAddActivity.java
+++ app/src/main/java/com/njordfjallen/offs3t/hobomaps/checklist/ChecklistAddActivity.java
@@ -7,7 +7,73 @@ import android.view.View;
 
 import com.njordfjallen.offs3t.hobomaps.R;
 
+/**
+ * The activity that allows a user to create a new Checklist Item.
+ *
+ * A Checklist Item represents
+ * a destination or recreational activity
+ * that interests the user.
+ *
+ * A Checklist Item represents
+ * a stop that the user wants to make
+ * during their roadtrip.
+ *
+ * Constants                                {@link #TAG}<br>
+ * Member variables
+ * UI elements
+ *
+ * Android Activity lifecycle methods       {@link #onCreate(Bundle)}<br>
+ * Initialization methods                   {@link #getUiHooks()}<br>
+ * Methods that respond to user clicks
+ * Methods that read from the database
+ * Methods that write to the database
+ * Methods that manage the options menu
+ */
 public class ChecklistAddActivity extends AppCompatActivity {
+
+    /* Constants */
+
+    /**
+     * Android logging tag
+     */
+    private static final String TAG = "checklist/ChecklistAddActivity";
+
+    /**
+     * An Intent extra key.
+     * If this key is received, this activity will edit an existing Checklist Item
+     * instead of creating a new one (when the user fills-in the initially-blank fields.
+     * This key is used to pass the row ID of the Checklist Item that should be
+     * displayed, made editable, and overwritten with the user's new values.
+     */
+    public static final String ROW_ID_KEY = "RowID";
+
+    /**
+     * The value for the row ID if a value was not received as an Intent extra
+     */
+    private static final long ROW_ID_NOT_RECEIVED = -1;
+
+    /**
+     * An Intent extra key.
+     * If this key is received, this activity will set the focus to the View
+     * that has the <code>int</code> ID that was given.
+     */
+    public static final String VIEW_FOCUS_KEY = "ViewFocus";
+
+    /**
+     * The maximum value allowable in the quantity number picker
+     * TODO Currently un-used, need to change to value that will make sense
+     */
+    public static final int NUMBER_PICKER_MAX_VALUE = 999;
+
+
+
+    /* Member variables */
+
+    // TODO
+
+
+
+    /* Android lifecycle methods */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
