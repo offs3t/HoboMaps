b366f4b23400d9616e28e6a318945a08dfe82a12
offs3t <quavoyrn2016@gmail.com>
2016-06-08 04:01:32 -0700

    Add Java class that includes useful String constants

---
+++ app/src/main/java/com/njordfjallen/offs3t/hobomaps/db/DbStuff.java
@@ -0,0 +1,11 @@
+/**
+ * Sup
+ */
+public class DbStuff {
+
+    static final String TEXT_TYPE = " TEXT";
+    static final String BOOLEAN_TYPE = " BOOLEAN";
+    static final String INTEGER_TYPE = " INTEGER";
+    static final String COMMA = ",";
+
+}
